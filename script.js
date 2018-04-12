2f29ce92947b1a299e3fc50afe57375b26e7c36f
Shaleila <shaleila.louis@gmail.com>
2018-04-11 19:32:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-$(function() {
-  $('.pop-up').hide();
-  $('.pop-up').fadeIn(1000);
+(function() {
+  ('.pop-up').hide();
+  ('.pop-up').fadeIn(1000);
   
-      $('.close-button').click(function (e) { 
+      ('.close-button').click(function (e) { 
 
-      $('.pop-up').fadeOut(700);
-      $('#overlay').removeClass('blur-in');
-      $('#overlay').addClass('blur-out');
+      ('.pop-up').fadeOut(700);
+      ('#overlay').removeClass('blur-in');
+      ('#overlay').addClass('blur-out');
       e.stopPropagation();
         
     });
